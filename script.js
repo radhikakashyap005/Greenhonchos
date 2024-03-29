39f56100ec834b80ea8888fe0026ded7d99e6b2e
Radhika <radhikakashyap005>
2024-03-28 20:54:53 -0700

    modal

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const searchInput = document.getElementById("searchInput");
-    const searchButton = document.getElementById("searchButton");
-
-    searchButton.addEventListener("click", function() {
-        searchInput.classList.toggle("d-none");
-        searchInput.focus();
-    });
-});
